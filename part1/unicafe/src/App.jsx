1b196258e931227960bf8d6e48db6606d41c131a
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-01 11:24:35 -0300

    add exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,24 +1,12 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const handleGoodClick = () => setGood(good + 1)
-  const handleNeutralClick = () => setNeutral(neutral + 1)
-  const handleBadClick = () => setBad(bad + 1)
-
+const Statistics = ({ good, bad, neutral }) => {
   const total = good + neutral + bad
   const average = (good - bad) / total
   const positivePercentage = (good / total) * 100
 
   return (
-    <main>
-      <h1>Give Feedback</h1>
-      <button onClick={handleGoodClick}>good</button>
-      <button onClick={handleBadClick}>bad</button>
-      <button onClick={handleNeutralClick}>neutral</button>
+    <div>
       <h2>Statistics</h2>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
@@ -29,6 +17,26 @@ const App = () => {
         Positive Percentage:{' '}
         {isNaN(positivePercentage) ? 0 : positivePercentage}%
       </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
+  return (
+    <main>
+      <h1>Give Feedback</h1>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleBadClick}>bad</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </main>
   )
 }
